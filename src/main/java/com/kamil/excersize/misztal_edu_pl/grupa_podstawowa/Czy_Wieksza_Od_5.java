bda0f3dd2eead5e02dbdbe6de2a1ace6896a2264
KamilS123 <k.superson@gmail.com>
2019-01-08 00:27:47 +0100

    misztal excersize added

---
+++ src/main/java/com/kamil/excersize/misztal_edu_pl/grupa_podstawowa/Czy_Wieksza_Od_5.java
@@ -0,0 +1,37 @@
+package com.kamil.excersize.misztal_edu_pl.grupa_podstawowa;
+
+import java.util.Scanner;
+
+public class Czy_Wieksza_Od_5 {
+    public static void main(String[] args) {
+        System.out.println(isGraterThan(10));
+        System.out.println(isGrater());
+    }
+    private static Boolean isGraterThan(Integer number) {
+        Boolean result = false;
+        if (number>5) {
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
+    }
+    private static Boolean isGrater() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe: ");
+        Integer number = scanner.nextInt();
+
+        Boolean result = false;
+        if (number>5) {
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+}
+/*Zadanie 1. Proszę przygotować program który sprawdzi czy liczba jest większa od 5. Liczbę proszę:
+
+wczytać ze zmiennej,
+wczytać od użytkownika wykorzstując klasę Scanner.*/
